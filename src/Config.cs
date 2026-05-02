bb0a020484998b0a9d1eb6c4fc57bdb4914aca73
61852144+SainteKhalifa@users.noreply.github.com <61852144+SainteKhalifa@users.noreply.github.com>
2026-05-02 12:12:41 +0200

    nettoyage code

--- src/Config.cs
+++ src/Config.cs
@@ -5,9 +5,6 @@ namespace LuckyDefuse
 
     public class PluginConfig : BasePluginConfig
     {
-        [JsonPropertyName("planter_menu_duration")]
-        public int PlanterMenuDuration { get; set; } = 10;
-
         [JsonPropertyName("notification_delay")]
         public int NotificationDelay { get; set; } = 30;
 
